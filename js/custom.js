20f636375a10671bcb60e8eaa583a81602b948d7
asamisellah <asami.rachael@gmail.com>
2018-06-27 12:48:39 +0300

    [Feature #158418810] Use fetch api to log in a user

--- js/custom.js
+++ js/custom.js
@@ -22,30 +22,64 @@ function rejectButton() {
 // Fetch API
 
 // SignUp user
-document.getElementById("signup_form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  data = {
-    email: document.getElementById("email").value,
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-    confirm_password: document.getElementById("confirm_password").value
-  };
-  fetch("http://localhost:5000/api/v1/auth/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .then(data => {
-      if (data.message === "Sign Up Successful") {
-        window.location.href = "../pages/signin.html";
-      } else {
-        document.getElementById("message").innerHTML = data.message;
-        document.getElementById("message").style.color = "red";
-        document.getElementById("message").style.fontSize = "1.3rem";
-      }
+let signup = document.getElementById("signup_form");
+if (signup) {
+  signup.addEventListener("submit", function(e) {
+    e.preventDefault();
+    data = {
+      email: document.getElementById("email").value,
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      confirm_password: document.getElementById("confirm_password").value
+    };
+    fetch("http://localhost:5000/api/v1/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
     })
-    .catch(error => console.error("Error:", error));
-});
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === "Sign Up Successful") {
+          window.location.href = "../pages/signin.html";
+        } else {
+          document.getElementById("message").innerHTML = data.message;
+          document.getElementById("message").style.color = "red";
+          document.getElementById("message").style.fontSize = "1.3rem";
+        }
+      })
+      .catch(error => console.error("Error:", error));
+  });
+}
+
+// Login user
+let login = document.getElementById("login_form");
+if (login) {
+  login.addEventListener("submit", function(e) {
+    e.preventDefault();
+    data = {
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value
+    };
+    fetch("http://localhost:5000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === "Sign in Successful!") {
+          console.log(data.token);
+          // window.location.href = "../pages/signin.html";
+        } else {
+          document.getElementById("message").innerHTML = data.message;
+          document.getElementById("message").style.color = "red";
+          document.getElementById("message").style.fontSize = "1.3rem";
+        }
+      })
+      .catch(error => console.error("Error:", error));
+  });
+}
